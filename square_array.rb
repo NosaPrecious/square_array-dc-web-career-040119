5344abbf20fe013248e218fa1797589e68b1e8a8
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-15 15:31:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   arr_size= array.size
   count = 0
   array.each do |val|
-    sq_array<<val ** 2
+    sq_array<<val.sqrt
     count += 1
     array[count]= sq_array[count]
   end
